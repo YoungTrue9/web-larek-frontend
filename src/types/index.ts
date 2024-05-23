6bf9787df6120efa5f907d4078145b4fd4e008ce
Gleb <79607720+YoungTrue9@users.noreply.github.com>
2024-05-23 16:02:20 +0200

    Update index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,11 +1,11 @@
 // интерфейс который в себе хранит: описание, заголовок, категорию, изображение и цену. Нужен для хранения данных товара
     export interface IProductItem {
-    id: string;
     title: string;
     description: string;
     category: string;
     image: string;
     price: number | null;
+    id: string;
     }
 
 // обертка нужная для собрания продуктов IProductItem. 
@@ -18,8 +18,8 @@
     export interface IOrderForm {
     payment?: string;
     address?: string;
-    phone?: string;
     email?: string;
+    phone?: string;
     total?: string | number;
     }
 // на основе существущего интерфейса создаем другой, обертку в которой все это будет хранится.
@@ -29,12 +29,12 @@
   
 // интерфейс который нужен для того чтобы содержать в себе данные страницы и методы
     export interface IAppState {
+    loading: boolean;
     catalog: IProductItem[];
     preview: string;
-    basket: string[];
     order: IOrder;
+    basket: string[];
     total: string | number;
-    loading: boolean;
     }
 
 // данный FormErrors нужен для валидации ошибки
