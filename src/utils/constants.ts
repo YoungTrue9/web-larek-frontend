462c18803cda1865a9e099cc7c4aefa227f7a9b9
Gleb <79607720+YoungTrue9@users.noreply.github.com>
2024-05-24 00:37:56 +0200

    Update constants.ts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,16 @@
 export const API_URL = `${process.env.API_ORIGIN}/api/weblarek`;
 export const CDN_URL = `${process.env.API_ORIGIN}/content/weblarek`;
 
-export const settings = {
+interface ICategoryClassNames {
+	[categoryName: string]: string;
+}
 
+export const settings = {
+	categoryClassNames: {
+		'софт-скил': 'card__category_soft',
+		другое: 'card__category_other',
+		дополнительное: 'card__category_additional',
+		кнопка: 'card__category_button',
+		'хард-скил': 'card__category_hard',
+	} as ICategoryClassNames,
 };
