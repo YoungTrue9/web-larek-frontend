2f16910fa670127ad6241fc36e858c2d5026264c
Gleb <79607720+YoungTrue9@users.noreply.github.com>
2024-05-24 18:09:18 +0200

    Update Basket.ts

--- src/components/Basket.ts
+++ src/components/Basket.ts
@@ -1,6 +1,7 @@
-import { createElement } from '../utils/utils';
+import { createElement, ensureElement } from '../utils/utils';
 import { Component } from './base/Component';
 import { EventEmitter } from './base/Events';
+
 interface IBasket {
 	items: HTMLElement[];
 	total: number;
@@ -14,7 +15,7 @@ export class Basket extends Component<IBasket> {
 	constructor(container: HTMLElement, protected events: EventEmitter) {
 		super(container);
 
-		this._list = this.container.querySelector('.basket__list');
+		this._list = ensureElement<HTMLElement>('.basket__list', this.container);
 		this._total = this.container.querySelector('.basket__price');
 		this.button = this.container.querySelector('.basket__button');
 
@@ -41,63 +42,5 @@ export class Basket extends Component<IBasket> {
 
 	set total(total: number) {
 		this.setText(this._total, `${total} синапсов`);
-	}
-
-    toggleButton(isDisabled: boolean) {
-		this.setDisabled(this.button, isDisabled)
-	}
-}
-
-export interface IProduct {
-    id: string;
-    description: string;
-    image: string;
-    title: string;
-    category: string;
-    price: number | null;
-      selected: boolean;
-  }
-  export interface IProductItemBasketActions {
-	onClick: (event: MouseEvent) => void;
-}
-export interface IProductInBasket extends IProduct {
-	index: number;
-}
-  export class ProductItemBasket extends Component<IProductInBasket> {
-	protected _index: HTMLElement;
-	protected _title: HTMLElement;
-	protected _price: HTMLElement;
-	protected _button: HTMLButtonElement;
-
-	constructor(
-		protected blockName: string,
-		container: HTMLElement,
-		actions?: IProductItemBasketActions
-	) {
-
-		super(container);
-		this._title = container.querySelector(`.${blockName}__title`);
-		this._index = container.querySelector(`.basket__item-index`);
-		this._price = container.querySelector(`.${blockName}__price`);
-		this._button = container.querySelector(`.${blockName}__button`);
-
-		if (this._button) {
-			this._button.addEventListener('click', (evt) => {
-				this.container.remove();
-				actions?.onClick(evt);
-			});
-		}
-	}
-
-	set title(value: string) {
-		this.setText(this._title, value);
-	}
-
-	set index(value: number) {
-		this.setText(this._index, value);
-	}
-
-	set price(value: number) {
-		this.setText(this._price, value + ' синапсов');
 	}
 }
