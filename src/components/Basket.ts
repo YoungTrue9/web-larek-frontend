9b4fc7a120518ae1b2a829220e641e0d5fb2cf49
Gleb <79607720+YoungTrue9@users.noreply.github.com>
2024-05-24 02:41:57 +0200

    Update Basket.ts

--- src/components/Basket.ts
+++ src/components/Basket.ts
@@ -43,4 +43,62 @@ export class Basket extends Component<IBasket> {
 	set total(total: number) {
 		this.setText(this._total, `${total} синапсов`);
 	}
+
+    toggleButton(isDisabled: boolean) {
+		this.setDisabled(this.button, isDisabled)
+	}
+}
+
+export interface IProduct {
+    id: string;
+    description: string;
+    image: string;
+    title: string;
+    category: string;
+    price: number | null;
+      selected: boolean;
+  }
+  export interface IProductItemBasketActions {
+	onClick: (event: MouseEvent) => void;
+}
+export interface IProductInBasket extends IProduct {
+	index: number;
+}
+  export class ProductItemBasket extends Component<IProductInBasket> {
+	protected _index: HTMLElement;
+	protected _title: HTMLElement;
+	protected _price: HTMLElement;
+	protected _button: HTMLButtonElement;
+
+	constructor(
+		protected blockName: string,
+		container: HTMLElement,
+		actions?: IProductItemBasketActions
+	) {
+
+		super(container);
+		this._title = container.querySelector(`.${blockName}__title`);
+		this._index = container.querySelector(`.basket__item-index`);
+		this._price = container.querySelector(`.${blockName}__price`);
+		this._button = container.querySelector(`.${blockName}__button`);
+
+		if (this._button) {
+			this._button.addEventListener('click', (evt) => {
+				this.container.remove();
+				actions?.onClick(evt);
+			});
+		}
+	}
+
+	set title(value: string) {
+		this.setText(this._title, value);
+	}
+
+	set index(value: number) {
+		this.setText(this._index, value);
+	}
+
+	set price(value: number) {
+		this.setText(this._price, value + ' синапсов');
+	}
 }
