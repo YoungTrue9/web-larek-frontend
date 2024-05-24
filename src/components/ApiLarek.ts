90fe8a1ac8c5b7617a5aebe5df90b5a955724f2b
Gleb <79607720+YoungTrue9@users.noreply.github.com>
2024-05-24 18:08:38 +0200

    Update ApiLarek.ts

--- src/components/ApiLarek.ts
+++ src/components/ApiLarek.ts
@@ -1,25 +1,22 @@
-import { Api, ApiListResponse} from "./base/Api";
-import { IOrder,  IProductItem , IOrderResult } from "../types";
+import { Api, ApiListResponse } from './base/Api';
+import { IOrder, IProductItem, IOrderResult } from '../types';
 
 // Основной главный класс работующий в проекте с сетью, расширение базового класса Api.
 export class ApiLarek extends Api {
-  cdn: string;
+	cdn: string;
 
-  constructor(cdn: string, baseUrl: string, options?: RequestInit) {
-    super(baseUrl, options)
-    this.cdn = cdn; // наш URL
-  }
-  // метод для получения списка товаров с сервера
-  getProductList() {
-    return this.get('/product')
-      .then((data: ApiListResponse<IProductItem>) => {
-        return data.items.map((item) => ({ ...item }))
-      })
-  }
-  // метод для отправки данных заказов на серверa
-  orderProducts(order: IOrder): Promise<IOrderResult> {
-    return this.post('/order', order).then(
-        (data: IOrderResult) => data
-    );
-  }
+	constructor(cdn: string, baseUrl: string, options?: RequestInit) {
+		super(baseUrl, options);
+		this.cdn = cdn; // наш url
+	}
+	// метод для получения списка товаров с сервера
+	getProductList() {
+		return this.get('/product').then((data: ApiListResponse<IProductItem>) => {
+			return data.items.map((item) => ({ ...item }));
+		});
+	}
+	// метод для отправки данных заказов на сервер
+	orderProducts(order: IOrder): Promise<IOrderResult> {
+		return this.post('/order', order).then((data: IOrderResult) => data);
+	}
 }
